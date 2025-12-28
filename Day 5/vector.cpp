051bd210a5eedf7af8ad56d91f03f8a1f6dcfa65
TereshaB <tereshabhatt@gmail.com>
2025-12-28 05:54:25 +0545

    Basic syntax of vector

---
+++ Day 5/vector.cpp
@@ -0,0 +1,12 @@
+// How to initialize vector?
+#include<vector>
+#include<iostream>
+using namespace std;
+int main()
+{
+    vector <int> vec;   // one way of declaring vector
+    vector <float> vect ={1,2.3,4.5};
+    vector <string> th(3,"Teresha"); // this will make a vector of size 3 and Teresha will be printed 3 times 
+
+}
+
